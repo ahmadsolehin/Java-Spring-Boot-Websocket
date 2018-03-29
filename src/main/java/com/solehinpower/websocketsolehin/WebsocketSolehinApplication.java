eecc837ef44ed921e90e4b0210290dfdd26328a0
admin <admin@Solehin-MacBook-Pro.local>
2018-03-29 23:26:23 +0800

    haha

---
+++ src/main/java/com/solehinpower/websocketsolehin/WebsocketSolehinApplication.java
@@ -0,0 +1,12 @@
+package com.solehinpower.websocketsolehin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSolehinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSolehinApplication.class, args);
+	}
+}
